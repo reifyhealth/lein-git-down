49bc0cb577998813aa3dbb25b57734573a42bb4b
Matt Anderson <manderson202@gmail.com>
2020-11-22 22:07:36 -0500

    Update dependencies and project version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject reifyhealth/lein-git-down "0.3.7"
+(defproject reifyhealth/lein-git-down "0.4.0"
   :description "A Leiningen plugin for resolving Clojure(Script) dependencies from a Git repository"
   :url "http://github.com/reifyhealth/lein-git-down"
   :license {:name "MIT"}
-  :dependencies [[org.clojure/tools.gitlibs "0.2.64"
+  :dependencies [[org.clojure/tools.gitlibs "1.0.100"
                   :exclusions [org.apache.httpcomponents/httpclient
                                org.slf4j/slf4j-api]]
-                 [leiningen "2.8.1" :scope "provided"]]
+                 [leiningen "2.9.4" :scope "provided"]]
   :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                     :username :env/clojars_username
                                     :password :env/clojars_password
